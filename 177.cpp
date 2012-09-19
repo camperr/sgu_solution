371785fa2821d75526c29befd349687e7d2ceaf7
zhang nan <zhangnan.up@gmail.com>
2012-09-19 11:11:33 +0800

    one-dimension version

--- 177.cpp
+++ 177.cpp
@@ -10,7 +10,7 @@ const int MAXN=3000;
 int n,m;
 class Node{
     public:
-        int value;
+        int val;
         int low,high;
 };
 class SegmentTree{
@@ -19,97 +19,80 @@ class SegmentTree{
         SegmentTree(){
             memset(tr,-1,sizeof(tr));
         }
+        void push(int c){
+            if(tr[c].val!=-1){
+                tr[c<<1].val=tr[c<<1|1].val=tr[c].val;
+                tr[c].val=-1;
+            }
+        }
+        void pull(int c){
+            if(tr[c<<1].val!=-1 && tr[c<<1].val==tr[c<<1|1].val){
+                tr[c].val=tr[c<<1].val;
+            }     
+        }
         void build(int l,int r,int c,int val){
             tr[c].low=l;
             tr[c].high=r;
             tr[c].val=val;
             if(l>=r) return;
             int mid=(l+r)>>1;
-            build(l,mid,c<<1);
-            build(mid+1,r,c<<1|1);
+            build(l,mid,c<<1,val);
+            build(mid+1,r,c<<1|1,val);
         }
         void update(int l,int r,int c,int val){
             if(l==tr[c].low && r==tr[c].high){
-                tr[c].value=val;
+                tr[c].val=val;
                 return;
             } 
-            int mid=(tr[c].l+tr[c].r)>>1;
+            if(tr[c].low<=l && r<=tr[c].high && tr[c].val==val) return;
+            int mid=(tr[c].low+tr[c].high)>>1;
+            push(c);
             if(r<=mid) update(l,r,c<<1,val);
             else if(l>mid) update(l,r,c<<1|1,val);
             else{
                 update(l,mid,c<<1,val);
                 update(mid+1,r,c<<1|1,val);
             }
+            pull(c);
         }
-        int query(int l,int r,int c,int val){
-            if(l==tr[c].low && r==tr[c].high){
-                if(tr[c].val==val) return r-l+1;
+        int query(int val,int c){
+            if(tr[c].val!=-1){
+                if(tr[c].val==val) return tr[c].high-tr[c].low+1;
                 else return 0;
-            }
-            int mid=(tr[c].l+tr[c].r)>>1;
-            if(r<=mid) return query(l,r,c<<1,val);
-            else if(mid<l) return query(l,r,c<<1|1,val);
-            else return query(l,mid,c<<1,val)+query(mid+1,r,c<<1|1,val);
+            }else return query(val,c<<1)+query(val,c<<1|1);
         }
 };
-class Node2{
-    public:
-        SegmentTree st;
-        int l,int r;
-};
-
-class ST2{
-    public:
-    Node2 st2[MAXN];
-    void build(int u,int v,int l,int r,int c,int val){
-        st2[c].l=u;
-        st2[c].r=v;
-        st2[c].st.build(l,r,1,val);
-        if(u>=v) return;
-        int mid=(u+v)>>1;
-        build(u,mid,l,r,c<<1);
-        build(mid+1,v,l,r,c<<1|1);
-    }
-    void update(int u,int v,int l,int r,int c,int val){
-        if(st2[c].l==u && st2[c].r==v){
-            st2[c].st.update(l,r,1,val);
-            return;
-        }
-        int mid=(st2[c].l+st2[c].v)>>1;
-        if(v<=mid) update(u,v,l,r,c<<1,val);
-        else if(mid<u) update(u,v,l,r,c<<1|1,val);
-        else{
-            update(u,mid,l,r,c<<1,val);
-            update(mid+1,v,l,r,c<<1|1,val);
-        }
-    }
-    void query(int u,int v,int l,int r,int c,int val){
-        if(st2[c].l==u && st2[c].r==v){
-            return st2[c].st.query(l,r,1,val);
-        }
-        int mid=(st2[c].l+st2[c].r)>>1;
-        if(v<=mid) return query(u,v,l,r,c<<1,val);
-        else if(mid<u) return query(u,v,l,r,c<<1|1,val);
-        else return query(u,mid,l,r,c<<1,val)+query(mid+1,v,l,r,c<<1|1,val);
-    }
-};
-ST2 s2;
-void makedata(){
-    freopen("in.txt","r",stdin);
-    scanf("%d%d",&n,&m);
-    s2.build(1,n,1,n,1,0);
+SegmentTree st[MAXN];
+int cal(){
     int i,k;
-    int u,v,l,r;
-    for(i=0;i<m;i++){
-        char c;
-        scanf("%d%d%d%d %c",&u,&v,&l,&r,&c);
-        printf("%c\n",c);
-        if(c=='w') k=0;
-        else k=1;
-        s2.update(u,v,l,r,1,k);
+    for(k=0,i=1;i<=n;i++){
+        k+=st[i].query(0,1);
     }
+    return k;
+}
+void swap(int &u,int &v){
+    u^=v;
+    v^=u;
+    u^=v;
 }
 int main(){
-    makedata();
-    printf("%d\n",s2.query(1,n,1,n,1,0));
+    freopen("in.txt","r",stdin);
+    int i,j,k;
+    scanf("%d %d",&n,&m);
+    int rl,rr,cl,cr;
+    char c;
+    for(i=1;i<=n;i++) {
+        st[i].build(1,n,1,-1);
+        st[i].tr[1].val=0;
+    }
+    for(i=0;i<m;i++){
+        scanf("%d%d%d%d %c",&rl,&rr,&cl,&cr,&c);
+        if(c=='w') k=0;
+        else k=1;
+        if(rl>cl) swap(rl,cl);
+        if(rr>cr) swap(rr,cr);
+        for(j=rl;j<=cl;j++) st[j].update(rr,cr,1,k);
+    }
+    k=cal();
+    printf("%d\n",k);
 }
